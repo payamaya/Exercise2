0ba8f3aced2d26b0286dcc98c8b77f04b52a6d4a
payamaya <paulyashou81@gmail.com>
2024-05-27 13:14:22 +0200

    namespaces

--- Exercise/Program.cs
+++ Exercise/Program.cs
@@ -1,4 +1,4 @@
-﻿
+﻿/*
 using Exercise.Helpers;
 using Exercise.UI;
 using System;
@@ -29,4 +29,36 @@ internal class Program
 
 }
 
+}*/
+
+using Exercise.Helpers;
+using Exercise.UI;
+using System;
+
+
+using Exercise;
+//{
+
+
+internal class Program
+{
+
+    //  private static ConsoleUI _ui = new ConsoleUI() 
+    //  private static ConsoleUI _ui = new();
+
+    // Instantiate a class that implements the interface,BUT WE CAN PUT IT IN THE IUI 
+
+
+
+    static void Main()
+    {
+        IUI ui = new ConsoleUI();
+        Main main = new Main(ui);
+        main.Run();
+    }
+
+
+
 }
+
+//}
